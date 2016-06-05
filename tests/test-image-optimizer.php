530521a72c705ba2d45f40ebe9e6130282a4c869
maor <maorhaz@gmail.com>
2016-06-05 18:06:59 +0300

    Test additions

---
+++ tests/test-image-optimizer.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Class WSI_Test_Base
+ *
+ * @package 
+ */
+
+/**
+ * WSI_Test_Base
+ */
+class WSI_Test_Image_Optimizer extends WP_UnitTestCase {
+
+	/**
+	 * Test if instance is the same as it should be
+	 * 
+	 * @return [type] [description]
+	 */
+	function test_get_instance() {
+		$instance = winsite_image_optimizer();
+		$this->assertInstanceOf( 'Winsite_Image_Optimizer', $instance );
+	}
+
+	
+}
