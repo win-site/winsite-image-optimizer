e962273089df9d561b56b3a389ca0cf4f03b0baf
maor <maorhaz@gmail.com>
2016-06-06 15:29:34 +0300

    Added settings

--- includes/class-wsi-retro-processor.php
+++ includes/class-wsi-retro-processor.php
@@ -1,29 +1,53 @@
 <?php
 
 class WSI_Retro_Processor {
+	private $settings_page = 'media';
+	private $settings_section = 'wsi_media_settings';
+
+	private static $settings = array(); // use $this->get_settings()
+
 	public function __construct() {
 		// Load up the localization file if we're using WordPress in a different language
 		load_plugin_textdomain( 'wsi-image-optimizer' );
 
-
-		add_action( 'admin_menu', 			   array( $this, 'add_admin_menu' ) );
+		add_action( 'admin_menu',              array( $this, 'add_admin_menu' ) );
+		add_action( 'admin_init',              array( $this, 'action_admin_init' ) );
 		add_action( 'wp_ajax_wsi-regeneretro', array( $this, 'do_regeneretro' ) );
-
 
 		// Allow people to change what capability is required to use this plugin
 		$this->capability = apply_filters( 'wsi_processor_page_cap', 'manage_options' );
 	}
 
+	public function action_admin_init() {
+		// Plugin setting section
+		register_setting( $this->settings_page, $this->settings_section, array( $this, 'sanitize_options' ) );
+
+		add_settings_section(
+			$this->settings_section,
+			__( 'Winsite Image Optimizer', 'winsite-images' ),
+			array( $this, 'settings_section_intro' ),
+			$this->settings_page
+		);
+
+		add_settings_field(
+			$this->settings_section . '-' . 'should-keep-original',
+			__( 'Keep original?', 'winsite-images' ),
+			array( $this, 'field_yesno' ),
+			$this->settings_page,
+			$this->settings_section,
+			array( 'option' => 'should-keep-original' )
+		);
+	}
+
 	public function add_admin_menu() {
 		// add_management_page
-		$this->menu_id = add_media_page( __( 'Winsite Image Optimizer', 'wsi-image-optimizer' ), __( 'WS. Image Optimizer', 'wsi-image-optimizer' ), $this->capability, 'wsi-image-optimizer', array($this, 'regenerate_interface') );
+		$this->menu_id = add_media_page( __( 'Winsite Image Optimizer', 'wsi-image-optimizer' ), __( 'WS. Image Optimizer', 'wsi-image-optimizer' ), $this->capability, 'wsi-image-optimizer', array( $this, 'regenerate_interface' ) );
 		add_action( "load-$this->menu_id", array( $this, 'page_load' ) ); // register hook
 	}
 
 	public function page_load() {
 		// Enqueue assets
 		wp_enqueue_script( 'wsi-main', plugins_url( 'assets/js/winsite-image-optimizer.js', dirname( __FILE__ ) ), array( 'jquery' ) );
-
 
 		$args = array(
 			'post_type'	 	 => 'attachment',
@@ -51,9 +75,51 @@ class WSI_Retro_Processor {
 			'ids' => $filtered_attachments,
 			'l10n' => array(
 				'alert_no_images' => __( 'There are no images to process at the moment. Yay!', 'winsite-images' ),
-			)
+			),
 		) );
 	}
+
+	/**
+	 * Display assistive text in settings section
+	 *
+	 * @uses _e
+	 * @uses this::plugin_priority
+	 * @return string
+	 */
+	public function settings_section_intro() {
+		?>
+		<p><?php _e( 'Control the way Winsite Image Optimizer works via the settings below.', 'wp_revisions_control' ); ?></p>
+		<?php
+	}
+
+	/**
+	 * Render field for each post type
+	 *
+	 * @param array $args
+	 * @uses this::get_revisions_to_keep
+	 * @uses esc_attr
+	 * @return string
+	 */
+	public function field_yesno( $args ) {
+		$value = $this->get_setting( $args['option'] ) === '1';
+		?>
+		<label class="tix-yes-no description"><input type="radio" name="<?php echo esc_attr( $this->settings_section . '[' . $args['option'] . ']' ); ?>" value="1" <?php checked( $value, true ); ?>> <?php _e( 'Yes', 'winsite-images' ); ?></label>
+		<label class="tix-yes-no description"><input type="radio" name="<?php echo esc_attr( $this->settings_section . '[' . $args['option'] . ']' ); ?>" value="0" <?php checked( $value, false ); ?>> <?php _e( 'No', 'winsite-images' ); ?></label>
+		<?php
+	}
+
+	/**
+	 * Sanitize plugin settings
+	 *
+	 * @param array $options
+	 * @return array
+	 */
+	public function sanitize_options( $options ) {
+		$options_sanitized = array();
+		// return $options_sanitized;
+		return $options;
+	}
+
 
 	public function regenerate_interface() {
 		?>
@@ -66,7 +132,7 @@ class WSI_Retro_Processor {
 
 				<ul class="wsi-file-list"></ul>
 
-				<?php wp_nonce_field('wsi-regeneretro') ?>
+				<?php wp_nonce_field( 'wsi-regeneretro' ) ?>
 				<button class="button button-primary" type="submit">Regenerate Retroactive</button>
 				<span class="status status-finished"><span class="dashicons dashicons-yes"></span> Finished</span>
 			</form>
@@ -93,11 +159,11 @@ class WSI_Retro_Processor {
 		$id = (int) $_REQUEST['id'];
 		$image = get_post( $id );
 
-		if ( ! $image || 'attachment' != $image->post_type || 'image/' != substr( $image->post_mime_type, 0, 6 ) )
-			die( json_encode( array( 'error' => sprintf( __( 'Failed resize: %s is an invalid image ID.', 'regenerate-thumbnails' ), esc_html( $_REQUEST['id'] ) ) ) ) );
+		if ( ! $image || 'attachment' != $image->post_type || 'image/' != substr( $image->post_mime_type, 0, 6 ) ) {
+			die( json_encode( array( 'error' => sprintf( __( 'Failed resize: %s is an invalid image ID.', 'regenerate-thumbnails' ), esc_html( $_REQUEST['id'] ) ) ) ) ); }
 
-		if ( ! current_user_can( $this->capability ) )
-			$this->die_json_error_msg( $image->ID, __( "Your user account doesn't have permission to resize images", 'regenerate-thumbnails' ) );
+		if ( ! current_user_can( $this->capability ) ) {
+			$this->die_json_error_msg( $image->ID, __( "Your user account doesn't have permission to resize images", 'regenerate-thumbnails' ) ); }
 
 		if ( get_post_meta( $image->ID, '_wsi_photonized', true ) === '1' ) {
 			$this->die_json_error_msg( $image->ID, __( 'This image has already been processed.', 'regenerate-thumbnails' ) );
@@ -107,10 +173,10 @@ class WSI_Retro_Processor {
 		$fullsizeurl = wp_get_attachment_url( $image->ID );
 
 		// make sure we're not going to run the other actions
-		remove_filter( 'wp_handle_upload', array(winsite_image_optimizer()->hooks, 'filter_wp_handle_upload') );
+		remove_filter( 'wp_handle_upload', array( winsite_image_optimizer()->hooks, 'filter_wp_handle_upload' ) );
 
 		// Sideload media now
-		$overrides = array('test_form'=>false);
+		$overrides = array( 'test_form' => false );
 		$time = current_time( 'mysql' );
 
 		// sanitize file name
@@ -122,10 +188,10 @@ class WSI_Retro_Processor {
 		);
 
 		// If error storing temporarily, return the error.
-        if ( is_wp_error( $file_array['tmp_name'] ) ) {
-        	error_log( 'WSI: Error loading a file. Orig Array: ' . print_r(array( $file_array ), true) . "\n Error details: " . print_r( $file_array['tmp_name'], true ) );
-            $this->die_json_error_msg( $image->ID, __( 'Failed on line ' . __LINE__, 'regenerate-thumbnails' ) );
-        }
+		if ( is_wp_error( $file_array['tmp_name'] ) ) {
+			error_log( 'WSI: Error loading a file. Orig Array: ' . print_r( array( $file_array ), true ) . "\n Error details: " . print_r( $file_array['tmp_name'], true ) );
+			$this->die_json_error_msg( $image->ID, __( 'Failed on line ' . __LINE__, 'regenerate-thumbnails' ) );
+		}
 
 		$file = wp_handle_sideload( $file_array, $overrides, $time );
 
@@ -133,7 +199,7 @@ class WSI_Retro_Processor {
 		$ia = wp_insert_attachment( array(
 			'ID'             => $image->ID,
 			'guid'           => $file['url'],
-			'post_mime_type' => $file['type']
+			'post_mime_type' => $file['type'],
 		), $file['file'] );
 
 		// update in meta that it got processed
@@ -141,31 +207,29 @@ class WSI_Retro_Processor {
 
 		if ( file_exists( $fullsizepath ) ) {
 			update_post_meta( $image->ID, '_wsi_original_filesize', filesize( $fullsizepath ) );
-			
+
 			// Delete old image while we're at it
 			unlink( $fullsizepath );
 		}
 
 		update_post_meta( $image->ID, '_wsi_engine', (string) WSI_The_Golden_Retriever::get_engine( true ) );
 
-
 		// refresh our file variables
 		$fullsizepath = $file['file'];
 		$fullsizeurl = $file['url'];
 
-
-		if ( false === $fullsizepath || ! file_exists( $fullsizepath ) )
-			$this->die_json_error_msg( $image->ID, sprintf( __( 'The originally uploaded image file cannot be found at %s', 'regenerate-thumbnails' ), '<code>' . esc_html( $fullsizepath ) . '</code>' ) );
+		if ( false === $fullsizepath || ! file_exists( $fullsizepath ) ) {
+			$this->die_json_error_msg( $image->ID, sprintf( __( 'The originally uploaded image file cannot be found at %s', 'regenerate-thumbnails' ), '<code>' . esc_html( $fullsizepath ) . '</code>' ) ); }
 
 		@set_time_limit( 900 ); // 5 minutes per image should be PLENTY
 
 		$metadata = wp_generate_attachment_metadata( $image->ID, $fullsizepath );
 
-		if ( is_wp_error( $metadata ) )
-			$this->die_json_error_msg( $image->ID, $metadata->get_error_message() );
+		if ( is_wp_error( $metadata ) ) {
+			$this->die_json_error_msg( $image->ID, $metadata->get_error_message() ); }
 
-		if ( empty( $metadata ) )
-			$this->die_json_error_msg( $image->ID, __( 'Unknown failure reason.', 'regenerate-thumbnails' ) );
+		if ( empty( $metadata ) ) {
+			$this->die_json_error_msg( $image->ID, __( 'Unknown failure reason.', 'regenerate-thumbnails' ) ); }
 
 		// If this fails, then it just means that nothing was changed (old value == new value)
 		wp_update_attachment_metadata( $image->ID, $metadata );
@@ -175,5 +239,48 @@ class WSI_Retro_Processor {
 
 	public function die_json_error_msg( $id, $message ) {
 		die( json_encode( array( 'error' => sprintf( __( '&quot;%1$s&quot; (ID %2$s) failed to resize. The error message was: %3$s', 'regenerate-thumbnails' ), esc_html( get_the_title( $id ) ), $id, $message ) ) ) );
+	}
+
+
+	/**
+	 * * PLUGIN UTILITIES
+	 **/
+
+	/**
+	 * Retrieve plugin settings
+	 *
+	 * @uses this::get_post_types
+	 * @uses get_option
+	 * @return array
+	 */
+	private function get_settings() {
+		if ( empty( self::$settings ) ) {
+			$settings = get_option( $this->settings_section, array() );
+
+			if ( empty( $settings ) ) {
+				// Defaults
+				$settings = apply_filters( 'wsi_default_settings', array(
+					'should-keep-original' => '1',
+				) );
+
+				update_option( $this->settings_section, $settings );
+			}
+
+			self::$settings = $settings;
+		}
+		return self::$settings;
+	}
+
+	/**
+	 * Get a single setting
+	 *
+	 * @uses this::get_settings
+	 * @param  string         $key     Key of option to return
+	 * @param  boolean|string $default Default option to return if value not found
+	 * @return mixed           The option as stored in DB
+	 */
+	public function get_setting( $key, $default = false ) {
+		$settings = $this->get_settings();
+		return isset( $settings[ $key ] ) ? $settings[ $key ] : $default;
 	}
 }
